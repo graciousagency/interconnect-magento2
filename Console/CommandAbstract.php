b0991fc1d79934ddb0b583303ad7ae1190c9458b
Justin van Schaick <justin@graciousstudios.nl>
2017-10-12 12:58:17 +0200

    Removed unused dependancies

--- Console/CommandAbstract.php
+++ Console/CommandAbstract.php
@@ -2,7 +2,6 @@
 namespace Gracious\Interconnect\Console;
 
 use Exception;
-use Zend\EventManager\EventManager;
 use Magento\Framework\App\State;
 use Monolog\Handler\HandlerInterface;
 use Psr\Log\LoggerInterface as Logger;
@@ -14,9 +13,11 @@ use Magento\Customer\Model\ResourceModel\Customer;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Customer\Model\ResourceModel\Customer\Collection;
-use Zend\EventManager\EventManagerInterface;
-use Zend\Mvc\MvcEvent;
 
+/**
+ * Class CommandAbstract
+ * @package Gracious\Interconnect\Console
+ */
 abstract class CommandAbstract extends Command
 {
 
