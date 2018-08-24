c507c25fd77e6a61c2eeed19a73701ce3ef25e1c
Michael Willems <michael@graciousstudios.nl>
2018-08-24 10:15:16 +0200

    Added missing observer placeholder

---
+++ Observer/CheckoutCartProductAddAfter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Gracious\Interconnect\Observer;
+
+use Magento\Framework\Event\Observer;
+
+class CheckoutCartProductAddAfter extends Observer
+{
+
+    /**
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        return;
+    }
+}
